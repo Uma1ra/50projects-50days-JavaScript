7f00e6458b630dcd5e6624a4aafd03956cb5df9e
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-13 19:13:10 +0900

    [add] function to stop playing the sound and reset

--- 09_sound_board/script.js
+++ 09_sound_board/script.js
@@ -8,3 +8,12 @@ sounds.forEach(sound => {
 
   
 })
+
+function stopSongs() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound)
+
+    song.pause()
+    song.currentTime = 0
+  })
+}
