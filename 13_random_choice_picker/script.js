f0e69bfadbd4ca0a3e301f863d63819015914903
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-17 14:48:27 +0900

    [add] event to retrieve targeted value and pass onto createTags function

--- 13_random_choice_picker/script.js
+++ 13_random_choice_picker/script.js
@@ -1,6 +1,9 @@
 const tagsEl = document.getElementById("tags")
 const textarea = document.getElementById("textarea")
 
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value)
+})
 
 function createTags(input) {
   const tags = input.split(" ").filter(tag => tag.trim() !== "").map(tag => tag.trim());
