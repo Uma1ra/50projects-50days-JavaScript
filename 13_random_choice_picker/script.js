798ff3620bde9272364280b94c2f6ae2eb60b604
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-17 15:39:09 +0900

    [add] timeout function to set the textarea to blank after "Enter"

--- 13_random_choice_picker/script.js
+++ 13_random_choice_picker/script.js
@@ -3,6 +3,12 @@ const textarea = document.getElementById("textarea")
 
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value)
+
+  if(e.key === "Enter") {
+    setTimeout(()=> {
+      e.target.value = ""
+    }, 10)
+  }
 })
 
 function createTags(input) {
