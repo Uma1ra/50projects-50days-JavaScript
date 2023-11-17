a3127746f056c35b58c6794661a59fedfe6ebd3b
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-17 14:42:08 +0900

    [add] function to create tags
    - the split tags are in a loop
    - create span element
    - add tag class
    - tag is added as text
    - span is within the tags div

--- 13_random_choice_picker/script.js
+++ 13_random_choice_picker/script.js
@@ -4,6 +4,15 @@ const textarea = document.getElementById("textarea")
 
 function createTags(input) {
   const tags = input.split(" ").filter(tag => tag.trim() !== "").map(tag => tag.trim());
+
+  tagsEl.innerHTML = ""
+
+  tags.forEach(tag => {
+    const tagEl = document.createElement("span")
+    tagEl.classList.add("tag")
+    tagEl.innerText = tag
+    tagsEl.appendChild(tagEl)
+  });
 }
 
 function highlightTag(tag) {
