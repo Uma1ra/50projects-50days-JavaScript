3e1689b8662be71254672acfaca910794b5b63c0
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-20 21:18:25 +0900

    [add] highlighting small cups
    full class can be added and removed by clicking

--- 16_drink_water/script.js
+++ 16_drink_water/script.js
@@ -1,28 +1,28 @@
-const smallCups = document.querySelectorAll(".cups");
+const smallCups = document.querySelectorAll(".cup-small");
 const litres = document.getElementById("litres");
 const percentage = document.getElementById("percentage");
 const remaining = document.getElementById("remaining");
 
 smallCups.forEach((cup, idx) => {
-  cup.addEventListener("click", () => highlightsCups(idx));
+  cup.addEventListener("click", () => highlightCups(idx));
 })
 
-function highlightsCups(idx) {
-  if (idx === 7 && smallCups[idx].classList.contains("full"))idx--;
-  else if(smallCups[idx].classList.contains("full") && !smallCups[idx].nextElementSibling.contains("full")) {
+function highlightCups(idx) {
+  if (idx === 7 && smallCups[idx].classList.contains("full")) idx--;
+  else if(smallCups[idx].classList.contains("full") && !smallCups[idx].nextElementSibling.classList.contains("full")) {
     idx--;
   }
 
   smallCups.forEach((cup, idx2) => {
     if(idx2 <= idx) {
-      cup.classList.add("full")
+      cup.classList.add("full");
     } else {
-      cup.classList.remove("full")
+      cup.classList.remove("full");
     }
   })
 }
 
 function updateBigCup() {
-  const fullCups = document.querySelectorAll(".cup-small.full").length
-  const totalCups = smallCups.length
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
 }
