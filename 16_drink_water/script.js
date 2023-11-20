29c89f6c4cb75bd5a2fcf1b7460bbd2bb5664d2a
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-20 21:26:21 +0900

    [add] function to display the quantity of the big cup according to the small cups amount

--- 16_drink_water/script.js
+++ 16_drink_water/script.js
@@ -3,6 +3,8 @@ const litres = document.getElementById("litres");
 const percentage = document.getElementById("percentage");
 const remaining = document.getElementById("remaining");
 
+updateBigCup()
+
 smallCups.forEach((cup, idx) => {
   cup.addEventListener("click", () => highlightCups(idx));
 })
@@ -20,9 +22,28 @@ function highlightCups(idx) {
       cup.classList.remove("full");
     }
   })
+  
+  updateBigCup()
 }
 
 function updateBigCup() {
   const fullCups = document.querySelectorAll(".cup-small.full").length;
   const totalCups = smallCups.length;
+
+  if(fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${fullCups / totalCups * 330}px`;
+    percentage.innerText = `${fullCups / totalCups * 100}%`;
+  }
+
+  if(fullCups === totalCups) {
+    remaining.style.visibility = "hidden";
+    remaining.style.visibility = 0;
+  } else {
+    remaining.style.visibility = "visible";
+    litres.innerText = `${2 - (250 * fullCups / 1000)}L`;
+  }
 }
