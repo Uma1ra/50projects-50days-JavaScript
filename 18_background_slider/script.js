1fd2f31611892c47de0be0b7b8306cd9a57db925
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-26 23:57:03 +0900

    [add] function for bg-image and slide-image
    the slide index is determinded by the buttons and active class is removed overall before adding to the active slide and linking it to the bg

--- 18_background_slider/script.js
+++ 18_background_slider/script.js
@@ -11,6 +11,9 @@ rightButton.addEventListener("click", () => {
   if(activeSlide > slides.length - 1) {
     activeSlide = 0;
   }
+
+  setBgToBody();
+  setActiveSlide();
 })
 
 leftButton.addEventListener("click", () => {
@@ -19,4 +22,17 @@ leftButton.addEventListener("click", () => {
   if (activeSlide < 0) {
     activeSlide = slides.length - 1;
   }
+
+  setBgToBody();
+  setActiveSlide();
 })
+
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => slide.classList.remove("active"));
+
+  slides[activeSlide].classList.add("active")
+}
