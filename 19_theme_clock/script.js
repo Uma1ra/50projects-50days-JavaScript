0cd3205ae2cc1a2cf8d7f47a1431bc74ccf17603
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-27 23:14:25 +0900

    [add] defined html elements in js

---
+++ 19_theme_clock/script.js
@@ -0,0 +1,6 @@
+const toggleEl = document.querySelector(".toggle");
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
+const dateEl = document.querySelector(".date");
