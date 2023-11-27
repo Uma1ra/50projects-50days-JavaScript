5583147bd821ee3101e588ab65319c698559b27d
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-27 23:21:52 +0900

    [add] theme change function

--- 19_theme_clock/script.js
+++ 19_theme_clock/script.js
@@ -7,3 +7,15 @@ const dateEl = document.querySelector(".date");
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+toggleEl.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+
+  if(html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHtml = "Dark Mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHtml = "Light Mode";
+  }
+})
