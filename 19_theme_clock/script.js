44f702a44523c51f5798aade7db594c844173039
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-27 23:17:42 +0900

    [add] arrays for days and months

--- 19_theme_clock/script.js
+++ 19_theme_clock/script.js
@@ -4,3 +4,6 @@ const minuteEl = document.querySelector(".minute");
 const secondEl = document.querySelector(".second");
 const timeEl = document.querySelector(".time");
 const dateEl = document.querySelector(".date");
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
