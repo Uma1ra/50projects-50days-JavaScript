6f31a21e4023318dc4c910255e5fc2ec888fad33
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-11-30 01:24:08 +0900

    [add] click event and determining the button positions
    I've decided to write the event handler function outside of the event block for better readablity

---
+++ 20_button_ripple_effect/script.js
@@ -0,0 +1,16 @@
+const buttons = document.querySelectorAll(".ripple");
+
+function handleButtonClick(e) {
+  const x = e.pageX;
+  const y = e.pageY;
+  
+  const buttonTop = e.target.offsetTop;
+  const buttonLeft = e.target.offsetLeft;
+
+  const xInside = x - buttonLeft;
+  const yInside = y - buttonTop;
+}
+
+buutons.forEach(button => {
+  button.addEventListener("click", handleButtonClick);
+});
