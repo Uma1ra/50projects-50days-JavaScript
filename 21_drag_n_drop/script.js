49340d249e70fd8099def62be1555ccf392a9fb0
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-12-02 21:10:09 +0900

    [add] defining the dran and drop elements and functions in js

---
+++ 21_drag_n_drop/script.js
@@ -0,0 +1,17 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);\
+
+enpties.forEach(empty => {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", drop);
+});
+
+function dragStart() {
+  this.className += " hold"
+  setTimeout(() => this.className = "invisible", 0)
+}
