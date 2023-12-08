1c72576d2ef5f937b834c6ebcfa0aea17108aab1
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-12-09 00:48:29 +0900

    [update] changed variable naming method to conform to best practice

--- 24_content_placeholder/script.js
+++ 24_content_placeholder/script.js
@@ -1,7 +1,7 @@
 const header = document.getElementById("header");
 const title = document.getElementById("title");
 const excerpt = document.getElementById("excerpt");
-const profile_img = document.getElementById("profile_img");
+const profileImg = document.getElementById("profile_img");
 const authorName = document.getElementById("author-name");
 const date = document.getElementById("date");
 
@@ -15,7 +15,7 @@ function getData() {
   header.innerHTML = '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />';
   title.innerHTML = "This is the title";
   excerpt.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis";
-  profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/78.jpg" alt="" />';
+  profileImg.innerHTML = '<img src="https://randomuser.me/api/portraits/men/78.jpg" alt="" />';
   authorName.innerHTML = "John Doe";
   date.innerHTML = "Dec 09, 2023";
 };
