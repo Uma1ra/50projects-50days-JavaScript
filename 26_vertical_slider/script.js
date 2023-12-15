c19491e0391248773997a0ba7e336c1a98669981
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-12-16 01:03:07 +0900

    [add] set variables for slides, buttons, counted slides and reset the active slide count

---
+++ 26_vertical_slider/script.js
@@ -0,0 +1,9 @@
+const sliderContainer = document.querySelector(".slider-container");
+const slideLeft = document.querySelector(".left-slide");
+const slideRight = document.querySelector(".right-slide");
+const downButton = document.querySelector(".down-button");
+const upButton = document.querySelector(".up-button");
+
+const slidesLength = slideRight.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
