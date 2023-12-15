96a473aff7959e89921c1327fdb921b22579e315
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-12-16 01:30:01 +0900

    [add] function for slide movement

--- 26_vertical_slider/script.js
+++ 26_vertical_slider/script.js
@@ -13,3 +13,22 @@ slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 downButton.addEventListener("click", () => changeSlide("down"));
 upButton.addEventListener("click", () => changeSlide("up"));
 
+// using the arrow function as per ES6
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+
+  if(direction === "up") {
+    activeSlideIndex++;
+    if(activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  } else if(direction === "down") {
+    activeSlideIndex--;
+    if(activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1;
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
+  slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
+};
