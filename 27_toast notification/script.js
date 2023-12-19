813e7b3ce59528da9c25dceb9aa30e995b0b596a
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-12-20 00:29:51 +0900

    [add] defined necessary variables in js including some original notifications

---
+++ 27_toast notification/script.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("button");
+const toasts = document.getElementById("toast");
+
+const messages = [
+  "standard error",
+  "protocol error",
+  "spelling error",
+  "useful info",
+  "info loading",
+  "no info",
+  "saving successful",
+  "install success",
+  "download success"
+];
+
+const types = ["info", "success", "error"]
+
