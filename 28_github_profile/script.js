83966ec3a9ca8d6a775fcd34a4e41ad5678d618c
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-12-20 23:20:46 +0900

    [add] github api link

--- 28_github_profile/script.js
+++ 28_github_profile/script.js
@@ -1,3 +1,5 @@
+const APIURL = 'https://api.github.com/users/';
+
 const form = document.getElementById("form");
 const main = document.getElementById("main");
 const search = document.getElementById("search");
