d7ab0859e8902c86ce3e4d224dadd2be7a5adf4d
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-12-21 22:34:47 +0900

    [add] function for user info and profile image to convert into html elements

--- 28_github_profile/script.js
+++ 28_github_profile/script.js
@@ -17,3 +17,38 @@ async function getUser(username) {
     }
   }
 };
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + "/repos?sort=created");
+
+    addRepoToCard(data);
+  } catch(err) {
+    createErrorCard("Problem fetching repos");
+  }
+};
+
+function createUserCard(user) {
+  const userId = user.name || user.login;
+  const userBio = user.bio ? `<p>${user.bio}</p>`;
+  const cardHtml = `
+  <div class="card">
+    <div>
+      <img src="${user.avatar}" alt="${user.name}" class="avatar">
+    </div>
+
+    <div class="user-info">
+      <h2>${userId}</h2>
+      ${userBio}
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
+    </div>
+
+    <div id="repos"></div>
+  </div>
+  `;
+  main.innerHTML = cardHtml;
+};
